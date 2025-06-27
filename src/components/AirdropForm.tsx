d492c1b40e551d1ece68c809cbb21186c0ef10b5
garlicdoggoe <garlicspoofiyyy@gmail.com>
2025-06-27 17:59:18 +0800

    add(feats): send a transaction and details summary

--- src/components/AirdropForm.tsx
+++ src/components/AirdropForm.tsx
@@ -1,24 +1,93 @@
 "use client";
 
 import InputField from "./ui/InputField";
-import { useState } from "react";
+import Overlay from "./Overlay";
+import { useState, useEffect } from "react";
 import { chainsToTSender, tsenderAbi, erc20Abi } from "@/constants";
-import { useChainId, useConfig, useAccount, useWriteContract } from "wagmi";
+import { useChainId, useConfig, useAccount, useWriteContract, useReadContracts, useWaitForTransactionReceipt  } from "wagmi";
 import { readContract, waitForTransactionReceipt } from "@wagmi/core";
 import { useMemo } from "react";
-import { calculateTotal } from "@/utils";
+import { calculateTotal, formatTokenAmount } from "@/utils";
 
 function AirdropForm() {
   const [tokenAddress, setTokenAddress] = useState("");
   const [recipients, setRecipients] = useState("");
-  const [amount, setAmount] = useState("");
+  const [amounts, setAmounts] = useState("");
   const chainId = useChainId();
   const config = useConfig();
   const account = useAccount();
-  const total: number = useMemo(() => calculateTotal(amount), [amount]);
-  const { data: hash, isPending, writeContractAsync } = useWriteContract();
+  const total: number = useMemo(() => calculateTotal(amounts), [amounts]);
+  const { data: hash, isPending, error, writeContractAsync } = useWriteContract();
+  const { isLoading: isConfirming, isSuccess: isConfirmed, isError} = useWaitForTransactionReceipt({
+    confirmations: 1,
+    hash,
+  })
+  const [showSuccessOverlay, setShowSuccessOverlay] = useState(false);
+  const [showErrorOverlay, setShowErrorOverlay] = useState(false);
 
-  async function getApprovalAmount(
+  const { data: tokenData } = useReadContracts({
+    contracts: [
+      {
+        abi: erc20Abi,
+        address: tokenAddress as `0x${string}`,
+        functionName: "decimals",
+      },
+      {
+        abi: erc20Abi,
+        address: tokenAddress as `0x${string}`,
+        functionName: "name",
+      },
+      {
+        abi: erc20Abi,
+        address: tokenAddress as `0x${string}`,
+        functionName: "balanceOf",
+        args: [account.address],
+      },
+    ],
+  });
+  useEffect(() => {
+    if (isConfirmed) {
+      setShowSuccessOverlay(true);
+      const timer = setTimeout(() => {
+        setShowSuccessOverlay(false)
+      }, 3000);
+      return () => clearTimeout(timer);
+    }
+  }, [isConfirmed]);
+
+  useEffect(() => {
+    if (error) {
+      setShowErrorOverlay(true)
+      const timer = setTimeout(() => {
+        setShowErrorOverlay(false)
+      }, 3000);
+      return () => clearTimeout(timer);
+    }
+  }, [error]);
+
+  useEffect(() => {
+    const savedTokenAddress = localStorage.getItem('tokenAddress');
+    const savedRecipients = localStorage.getItem('recipients');
+    const savedAmounts = localStorage.getItem('amounts');
+
+    if (savedTokenAddress) setTokenAddress(savedTokenAddress);
+    if (savedRecipients) setRecipients(savedRecipients);
+    if (savedAmounts) setAmounts(savedAmounts);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('tokenAddress', tokenAddress);
+  }, [tokenAddress]);
+
+  useEffect(() => {
+    localStorage.setItem('recipients', recipients);
+  }, [recipients]);
+
+  useEffect(() => {
+    localStorage.setItem('amounts', amounts);
+  }, [amounts]);
+
+  async function getApprovalAmounts(
     tSenderAddress: string | null
   ): Promise<number> {
     if (!tSenderAddress) {
@@ -45,9 +114,10 @@ function AirdropForm() {
 
     // Get contract address
     const tSenderAddress = chainsToTSender[chainId]["tsender"];
-    const approvedAmount = await getApprovalAmount(tSenderAddress);
+    const approvedAmounts = await getApprovalAmounts(tSenderAddress);
     
-    if (approvedAmount < total) {
+    if (approvedAmounts < total) {
+      console.log("Approving tokens...");
       const approvalHash = await writeContractAsync({
         abi: erc20Abi,
         address: tokenAddress as `0x${string}`,
@@ -57,7 +127,7 @@ function AirdropForm() {
       const approvalReceipt = await waitForTransactionReceipt(config, {
         hash: approvalHash,
       });
-      console.log("approvalReceipt", approvalReceipt);
+      console.log("Approval confirmed: ", approvalReceipt);
 
       await writeContractAsync({
         abi: tsenderAbi,
@@ -66,57 +136,97 @@ function AirdropForm() {
         args: [
           tokenAddress,
           recipients.split(/[,\n]+/).map(addr => addr.trim()).filter(addr => addr !== ''),
-          amount.split(/[,\n]+/).map(amt => amt.trim()).filter(amt => amt !== ''),
+          amounts.split(/[,\n]+/).map(amt => amt.trim()).filter(amt => amt !== ''),
           BigInt(total),
         ],
       })
     } else {
-        await writeContractAsync({
-            abi: tsenderAbi,
-            address: tSenderAddress as `0x${string}`,
-            functionName: "airdropERC20",
-            args: [
-              tokenAddress,
-              recipients.split(/[,\n]+/).map(addr => addr.trim()).filter(addr => addr !== ''),
-              amount.split(/[,\n]+/).map(amt => amt.trim()).filter(amt => amt !== ''),
-              BigInt(total),
-          ],
-        })
+      await writeContractAsync({
+          abi: tsenderAbi,
+          address: tSenderAddress as `0x${string}`,
+          functionName: "airdropERC20",
+          args: [
+            tokenAddress,
+            recipients.split(/[,\n]+/).map(addr => addr.trim()).filter(addr => addr !== ''),
+            amounts.split(/[,\n]+/).map(amt => amt.trim()).filter(amt => amt !== ''),
+            BigInt(total),
+        ],
+      })
     }
   }
 
   return (
-    <div className="mx-auto max-w-5xl">
-      <InputField
-        label="Address"
-        placeholder="0x..."
-        value={tokenAddress}
-        type="text"
-        onChange={(e) => setTokenAddress(e.target.value)}
-      />
-      <InputField
-        label="Recipients"
-        placeholder="0x..., 0x..., 0x..."
-        value={recipients}
-        type="text"
-        onChange={(e) => setRecipients(e.target.value)}
-        large={true}
-      />
-      <InputField
-        label="Amount"
-        placeholder="100, 200, 300, ..."
-        value={amount}
-        type="text"
-        onChange={(e) => setAmount(e.target.value)}
-        large={true}
-      />
-      <button
-        onClick={handleSubmit}
-        className="cursor-pointer rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-      >
-        Send tokens
-      </button>
-    </div>
+    <>
+      {(isPending || isConfirming) && (
+        <Overlay
+          message={
+            isPending
+              ? "Submitting transaction..."
+              : "Confirming transaction..."
+          }
+        />
+      )}
+      {showSuccessOverlay && <Overlay message="Transaction successful!" />}
+      {showErrorOverlay && (
+        <Overlay 
+          message="Transaction aborted" 
+          type="error"
+        />
+      )}
+      <div className="mx-auto mb-10 max-w-5xl mt-10">
+        <InputField
+          label="ERC20 Address"
+          placeholder="0x..."
+          value={tokenAddress}
+          type="text"
+          onChange={(e) => setTokenAddress(e.target.value)}
+        />
+        <InputField
+          label="Recipients"
+          placeholder="0x..., 0x..., 0x..."
+          value={recipients}
+          type="text"
+          onChange={(e) => setRecipients(e.target.value)}
+          large={true}
+        />
+        <InputField
+          label="Amounts"
+          placeholder="100, 200, 300, ..."
+          value={amounts}
+          type="text"
+          onChange={(e) => setAmounts(e.target.value)}
+          large={true}
+        />
+        <div className="flex flex-col gap-2 p-4 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mb-5">
+          <h3 className="text-m font-medium pb-2">
+            Transaction Details
+          </h3>
+          <div className="flex justify-between mt-2">
+            <span className="text-gray-600 text-sm">Token Name</span>
+            <span className="font-mono">
+              {tokenData?.[1]?.result as string ?? "..."}
+            </span>
+          </div>
+          <div className="flex justify-between">
+            <span className="text-gray-600 text-sm">Amount (wei)</span>
+            <span className="font-mono">{total}</span>
+          </div>
+          <div className="flex justify-between">
+            <span className="text-gray-600 text-sm">Amount (tokens)</span>
+            <span className="font-mono">
+              {formatTokenAmount(total, tokenData?.[0]?.result as number)}
+            </span>
+          </div>
+        </div>
+        <button
+          onClick={handleSubmit}
+          disabled={isPending || isConfirming}
+          className="cursor-pointer rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:opacity-50 disabled:cursor-not-allowed"
+        >
+          Send tokens
+        </button>
+      </div>
+    </>
   );
 }
 
