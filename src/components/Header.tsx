e50ec7d30268312781f421a520d532a704d7bfcb
garlicdoggoe <garlicspoofiyyy@gmail.com>
2025-06-27 18:00:32 +0800

    update(): styling and removed github logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,13 @@
 "use client";
 
 import { ConnectButton } from "@rainbow-me/rainbowkit";
-import { FaGithub } from "react-icons/fa";
-import Image from "next/image";
 
 export default function Header() {
   return (
-    <nav className="px-8 py-4.5 border-b-[1px] border-zinc-100 flex flex-row justify-between items-center bg-white xl:min-h-[77px]">
+    <nav className="fixed top-0 left-0 right-0 z-50 px-8 py-4.5 border-b-[1px] border-zinc-100 flex flex-row justify-between items-center bg-white xl:min-h-[77px]">
       <div className="flex items-center gap-2.5 md:gap-6">
         <a href="/" className="flex items-center gap-1 text-zinc-800">
           <h1 className="font-bold text-2xl hidden md:block">Airdropper</h1>
-        </a>
-        <a
-          href="https://github.com/garlicdoggoe/airdropper"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="p-1 rounded-lg bg-zinc-900 hover:bg-zinc-800 transition-colors border-2 border-zinc-600 hover:border-zinc-500 cursor-alias hidden md:block"
-        >
-          <FaGithub className="h-5 w-5 text-white" />
         </a>
       </div>
       <div className="flex items-center gap-4">
